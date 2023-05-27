2befbf63a8f432bca1bca9f84b479df105d114b8
Aboubacar KAMARA <aboubacarkamara31@gmail.com>
2023-05-27 20:11:22 +0200

    ajout info page contact

--- lib/Page/pageContact.dart
+++ lib/Page/pageContact.dart
@@ -10,6 +10,36 @@ class PageContact extends StatefulWidget {
 
 class PageContactWidget extends State<PageContact> {
 
+  void showToast(){
+    print("Bouton copie numéro appuyé");
+  }
+
+  void redirectGoogle(){
+    print("Container appuyé");
+  }
+
+  Widget contactCard(){
+    return 
+    InkWell(
+      onTap: redirectGoogle,
+      child: Container(
+        child: Column(
+          children: [
+            const Text("Aboubacar",style: TextStyle(fontSize: 18)),
+            const Text("Kamara",style: TextStyle(fontSize: 18)),
+            Row(
+              children: [
+                const Text("07 69 67 64 04"),
+                IconButton(onPressed: showToast, icon: const Icon(Icons.copy))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,8 +53,8 @@ class PageContactWidget extends State<PageContact> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text("Page contact")
+          children: <Widget>[
+            contactCard()
           ]
         ),
       ),
